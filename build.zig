dca87f81d1d6a2cb66d5da75c4918582cf58add8
Evan Roggenkamp <evan.roggenkamp@arvig.com>
2025-01-30 15:35:31 -0600

    Downgraded Zig; fixed yaml

--- build.zig
+++ build.zig
@@ -14,56 +14,32 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
-
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
-    const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // We will also create a module for our other entry point, 'main.zig'.
-    const exe_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
-    exe_mod.addImport("zigBgp_lib", lib_mod);
-
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
-    const lib = b.addStaticLibrary(.{
-        .name = "zigBgp",
-        .root_module = lib_mod,
-    });
+    //
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "zigBgp",
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    b.installArtifact(lib);
+    // b.installArtifact(lib);
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
     const exe = b.addExecutable(.{
-        .name = "zigBgp",
-        .root_module = exe_mod,
+        .name = "zigBbgp",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
-
+    const yaml = b.dependency("yaml", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("yaml", yaml.module("yaml"));
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
@@ -95,13 +71,17 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const yaml = @import("yaml");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -12,9 +13,11 @@ pub fn main() !void {
 
     // Main loop
     try stdout.print("Entering main loop...\n", .{});
+    try bgp_daemon.load_config();
     while (true) {
         try bgp_daemon.run();
-        std.time.sleep(std.time.ns_per_s / 100); // Sleep for 100ms to avoid busy-looping
+        std.time.sleep(5 * std.time.ns_per_s);
+        // std.time.sleep(std.time.ns_per_s / 1000000); // Sleep for 100ms to avoid busy-looping
     }
 }
 
@@ -29,6 +32,45 @@ const BgpDaemon = struct {
 
     pub fn deinit(_: *BgpDaemon) void {
         // Clean up resources here if needed
+    }
+    pub fn load_config(_: *BgpDaemon) !void {
+        // Load config from file
+        var cwd = std.fs.cwd();
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
+        defer _ = gpa.deinit();
+        // Print the current working directory
+        const cwd_path = try cwd.realpathAlloc(allocator, ".");
+        defer allocator.free(cwd_path);
+        std.debug.print("Current working directory: {s}\n", .{cwd_path});
+        const cfg_file = try cwd.openFile("config.yaml", .{ .mode = .read_only });
+        // const cfg_file = try cwd.openFile("config.yaml", .{ .read = true });
+        defer cfg_file.close();
+        const file_content = try cfg_file.readToEndAlloc(allocator, std.math.maxInt(usize));
+        defer allocator.free(file_content);
+        var parsed_yaml = try yaml.Yaml.load(allocator, file_content);
+        defer parsed_yaml.deinit();
+        // Check if there's at least one document in the YAML
+        if (parsed_yaml.docs.items.len > 0) {
+            const map = parsed_yaml.docs.items[0].map;
+            // Assuming you want to get a key called "key_name"
+            if (map.get("as")) |value| {
+                // Here, 'value' would be the YAML node for "key_name".
+                // Depending on what type 'value' is, you might want to handle it differently:
+                switch (value) {
+                    .string => |str| {
+                        std.debug.print("The value of 'as' is: {s}\n", .{str});
+                    },
+                    .int => |int| {
+                        std.debug.print("The value of 'as' is: {}\n", .{int});
+                    },
+                    // Add more cases for other types if needed
+                    else => std.debug.print("The value of 'as' is of an unsupported type\n", .{}),
+                }
+            } else {
+                std.debug.print("'as' not found in the YAML\n", .{});
+            }
+        }
     }
 
     pub fn run(_: *BgpDaemon) !void {
